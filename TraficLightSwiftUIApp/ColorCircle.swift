1ca0316bca6342df8b9f9e75ffd6b0d0156a65df
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-09-08 00:41:43 +0300

    Design by SwiftUI

---
+++ TraficLightSwiftUIApp/ColorCircle.swift
@@ -0,0 +1,27 @@
+//
+//  ColorCircle.swift
+//  TraficLightSwiftUIApp
+//
+//  Created by Константин Прокофьев on 07.09.2021.
+//
+
+import SwiftUI
+
+struct ColorCircle: View {
+    let color: Color
+    var opacity: Double
+    var body: some View {
+        Circle()
+            .opacity(opacity)
+            .foregroundColor(color)
+            .frame(width: 150, height: 150)
+            .overlay(Circle().stroke(Color.white, lineWidth: 3))
+            .shadow(radius: 5)
+    }
+}
+
+struct ColorCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorCircle(color: .yellow, opacity: 0.2)
+    }
+}
--- TraficLightSwiftUIApp/ContentView.swift
+++ TraficLightSwiftUIApp/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+        Color.black.edgesIgnoringSafeArea(.all)
+        VStack {
+            VStack{
+                ColorCircle(color: .red, opacity: 0.2)
+                ColorCircle(color: .yellow, opacity: 0.2)
+                ColorCircle(color: .green, opacity: 0.2)
+            }
+            Spacer()
+            Button
+        }
+        }
+    }
+    
+    private var Button: some View {
+        SwiftUI.Button("Start"
+                       , action: {
+                        
+                       })
+            .font(.headline)
+            .foregroundColor(.white)
+            .background(Color.blue)
             .padding()
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
