2860ee965ffc2540302f9ad39444391fe427708a
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-09-07 23:18:28 +0300

    Initial Commit

---
+++ TraficLightSwiftUIApp/TraficLightSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraficLightSwiftUIAppApp.swift
+//  TraficLightSwiftUIApp
+//
+//  Created by Константин Прокофьев on 07.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct TraficLightSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
